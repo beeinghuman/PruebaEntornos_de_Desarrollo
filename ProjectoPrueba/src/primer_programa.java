e23d4a9b744b1f48d74ed346db9e6eda0f4b6e71
Bibi Osifo <bibiosifo@gmail.com>
2025-11-20 10:08:14 +0100

    Primera version del programa

---
+++ ProjectoPrueba/src/primer_programa.java
@@ -0,0 +1,20 @@
+
+public class primer_programa {
+	public static void main(String[] args) {
+		
+		int variable=5; 
+		variable++; 
+		variable+=10; 
+		System.out.println("Variable vale " + variable);
+		int contador = 0;
+		contador++;
+		
+		
+		
+		while (contador <= 10) {
+			System.out.println("El número es: " + contador);
+			contador++;
+		}
+
+	}
+}
